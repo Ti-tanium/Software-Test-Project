64e9c7f697f2b91ab4da8aed8a37a684586b2128
Ti-tanium <1158976264@qq.com>
2022-11-08 12:34:16 -0600

    flakeflagger prediction resuilt

--- FlakeFlagger/flakiness-predicter/processed_data.sh
+++ FlakeFlagger/flakiness-predicter/processed_data.sh
@@ -1,9 +1,9 @@
 # this to run generate the processsed_data from the features collections
 
 
-projects_dir="../../test-projects/"
+projects_dir="/misc/scratch/st_flaky_xx/projects/"
 source_of_flaky_tests="../../test-projects/historical_rerun_flaky_tests.csv"
 class_col="Test_class"
 test_method_col="Test_method"
 
-python generate_processed_data.py $projects_dir $source_of_flaky_tests $test_method_col $class_col
+python3 generate_processed_data.py $projects_dir $source_of_flaky_tests $test_method_col $class_col
