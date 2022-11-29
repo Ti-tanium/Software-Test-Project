095f3eff1fe624f12a9d6ef70664a04922337127
Ti-tanium <1158976264@qq.com>
2022-11-29 13:18:13 -0600

    update flakeflagger

--- test_script/run_flakify.sh
+++ test_script/run_flakify.sh
@@ -11,7 +11,8 @@ python3 -m pip install -r $FLAKIFY_DIR/requirements.txt
 
 echo "----------- Python packages install completed! ---------------"
 dataset_file="${WORK_DIR}/../test-projects/flakify_data.csv" 
-model_weights="${FLAKIFY_DIR}/results/Flakify_per_project_model_weights_on_${MODEL_WEIGHTS}_dataset.pt"
+# model_weights="${FLAKIFY_DIR}/results/Flakify_per_project_model_weights_on_${MODEL_WEIGHTS}_dataset.pt"
+model_weights="${FLAKIFY_DIR}/results/Flaify_model_trained_on_${MODEL_WEIGHTS}_dataset.pt"
 results_file="${WORK_DIR}/results/Flakify_cross_validation_results.csv"
 
 # python3 -u ${FLAKIFY_DIR}/src/Flakify_predictor_per_project.py $dataset_file $model_weights $results_file
